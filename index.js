51feda594dbfe2a8bf94e88ff50914b9692e9f74
Sally Bouley <sallybouley87@gmail.com>
2017-06-13 15:59:32 -0600

    stoplight, minus bonus

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-(function() {
-  'use strict';
-
-  // YOUR CODE HERE
-})();
+// (function() {
+//   'use strict';
+//
+//   // YOUR CODE HERE
+// })();
+var stopLight = document.getElementById('stopLight')
+var stopButton = document.getElementById('stopButton')
+stopButton.addEventListener('click', function(){
+  stopLight.classList.toggle('stop')
+})
+var slowLight = document.getElementById('slowLight')
+var slowButton = document.getElementById('slowButton')
+slowButton.addEventListener('click', function(){
+  slowLight.classList.toggle('slow')
+})
+var goLight = document.getElementById('goLight')
+var goButton = document.getElementById('goButton')
+goButton.addEventListener('click', function(){
+  goLight.classList.toggle('go')
+})
+var button = document.getElementsByClassName('button');
+for (var i=0;i<button.length; i++) {
+  button[i].addEventListener('mouseenter', function(){
+    console.log("Entered " + this.innerText + " button")
+  })
+}
+for (var i=0; i<button.length; i++) {
+  button[i].addEventListener('mouseleave', function(){
+    console.log("Left " + this.innerText + " button")
+  })
+}
